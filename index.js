3ffbe958624bb23516a91bb1e0f03856dc97d884
Zalman <moishkugel@gmail.com>
2022-09-02 12:33:05 -0400

    commit tree d4d6e296471a9b2f6fec6828e73f75b573e15494

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(res => res.json())
+    .then(newUser => {
+        const h1 = document.createElement('h1');
+        h1.textContent = newUser.id
+        document.body.appendChild(h1);
+    })
+    .catch(message => {
+        const p = document .createElement('p');
+        p.textContent = message
+        document.body.appendChild(p);
+    })
+}
